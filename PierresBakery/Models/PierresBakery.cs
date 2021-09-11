81fdbe1baf6ab8a32d4c795de77053498c7dd877
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-09-10 17:29:17 -0700

    add Main method with basic program functionality

--- PierresBakery/Models/PierresBakery.cs
+++ PierresBakery/Models/PierresBakery.cs
@@ -1,7 +1,19 @@
+using System;
+using PierresBakery;
+
 namespace PierresBakery
 {
   public class MyBakery
   {
+      public static void Main()
+      {
+        Console.WriteLine("Welcome to Pierre's Bakery || Home of Pierre's world-famous gluten-free pastries!");
+        Console.WriteLine("---------------------------------------------------------------------------------");
+        Console.WriteLine("Pierre's world-famous gluten-free pastries cost $2 a piece. Every third pastry is half off!");
+        Console.WriteLine("-------------------------------------------------------------------------------------------");
+        
+        Console.WriteLine("Please enter the number of pastries you'd like to purchase:");
   
+    }
   }
 }
