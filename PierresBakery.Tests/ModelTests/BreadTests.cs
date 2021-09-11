6673645e449d676a2cf80cc34a9f8f87d0d30c59
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-09-10 18:06:15 -0700

    add model and test files for Pastries and Bread

---
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery;
+
+namespace MyBakery.Tests
+{
+  [TestClass]
+  public class MyBakeryTests
+  {
+    // Test methods go here
+  }
+}
